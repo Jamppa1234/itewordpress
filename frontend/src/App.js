6a0421e9db504de195a5df50e8168ba74fcc75e4
Jamppa1234 <finjarnom@gmail.com>
2021-06-03 07:20:50 +0300

    lisätty books

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import Books from './components/Books';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <Books />
+       <h1>App</h1>
       </div>
     );
   }
--- frontend/src/components/Books.js
+++ frontend/src/components/Books.js
@@ -10,18 +10,19 @@ export class Books extends Component {
     componentDidMount() {
         axios.get('/wp-json/wp/v2/books')
             .then(res => this.setState({
-                books: res.data,
-                isLoaded: true
+                books: res.data
             }))
             .catch(err => console.log(err));
     }
 
     render() {
-        console.log(this.state);
         return (
-            <div />
+            <div>
+                
+            </div>
         )
     }
 }
 
 export default Books;
+
