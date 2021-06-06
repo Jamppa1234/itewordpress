868f8b4a6935a76fa36a997c9a67b67b0dfe58aa
Jamppa1234 <finjarnom@gmail.com>
2021-06-06 18:58:18 +0300

    luotu yhteys wp ja frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,32 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import {
+  Route,
+  NavLink,
+  BrowserRouter
+} from "react-router-dom";
+import Home from './components/Home';
+import Books from './components/Books';
+import Posts from './components/Posts';
 
+ 
 class App extends Component {
   render() {
     return (
-      <div className='App'>
-       <h1>App</h1>
-      </div>
+      <BrowserRouter>
+        <div>
+          <h1>Testisivusto (SPA)</h1>
+          <ul className="header">
+            <li><NavLink exact to="/">Home</NavLink></li>
+            <li><NavLink to="/books">Books</NavLink></li>
+            <li><NavLink to="/posts">Posts</NavLink></li>
+          </ul>
+          <div className="content">
+            <Route exact path="/" component={Home}/>
+            <Route path="/books" component={Books}/>
+            <Route path="/posts" component={Posts}/>
+          </div>
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- frontend/src/components/Books.js
+++ frontend/src/components/Books.js
@@ -1,28 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import axios from 'axios';
+import BookItems from './BookItems';
 
 export class Books extends Component {
-    state = {
-        books: [],
-        isLoaded: false
-    }
-
-    componentDidMount() {
-        axios.get('/wp-json/wp/v2/books')
-            .then(res => this.setState({
-                books: res.data
-            }))
-            .catch(err => console.log(err));
-    }
-
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+   state = {
+       books: [],
+       isLoaded: false
+   }
+ componentDidMount () {
+   axios.get('http://localhost:8000//wp-json/wp/v2/books/')
+       .then(res => this.setState({
+           books: res.data,
+           isLoaded: true
+       }))
+       .catch(err => console.log(err))
+   }
+   render() {
+    const {books, isLoaded} = this.state;
+    return (
+        <div>
+            {books.map(book =>
+            <BookItems key={book.id} book={book}/>
+            )}
+        </div>
+    );
 }
-
+}
 export default Books;
-
